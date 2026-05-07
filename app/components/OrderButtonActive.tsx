7474f24f57fb06f19128e1c8a73467a2de7645ef
Aleksey <avzag@yandex.ru>
2026-05-07 12:32:21 +0300

    глюк

--- app/components/OrderButtonActive.tsx
+++ app/components/OrderButtonActive.tsx
@@ -1,12 +1,16 @@
 "use client"
 
-import Link from "next/link";
-
 export default function OrderButtonActive ({ bgColor, textColor }: { bgColor: string, textColor: string }) {
   return (
-    <Link href="#showcase"
+    <button
+    onClick={(e) => {
+      if (window.location.pathname === '/') {
+        e.preventDefault();
+        document.getElementById('showcase')?.scrollIntoView({ behavior: 'smooth' });
+      }
+    }}
     className={`flex items-center justify-center w-[198] h-[60] text-base shadow-xl rounded-4xl lg:rounded-none ${bgColor} ${textColor}`}>
       Заказать букет
-    </Link>
+    </button>
   )
 }
